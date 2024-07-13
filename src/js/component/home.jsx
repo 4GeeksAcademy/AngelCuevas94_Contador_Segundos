50e512eedadac05f768afd99d5bb0b240f9bb42f
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-07-13 18:52:31 +0000

    estructura basica hecha

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,11 +8,14 @@ const Home = (props) => {
 	return (
 		<div className="text-center">
 			
-			<h1 className="text-center mt-5">{props.contador}</h1>
-			<h1 className="text-center mt-5">{props.contadormili}</h1>
-			
+			<h1 className="text-center mt-5">{props.decimasdesegundo}</h1>
+			<h1 className="text-center mt-5">{props.segundos}</h1>
+			<h1 className="text-center mt-5">{props.minutos}</h1>
+			<h1 className="text-center mt-5">{props.horas}</h1>
+
 
 		</div>
+		
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,22 +8,31 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let decimasdesegundo = 0
 let segundos = 0
-
-
+let minutos = 0
+let horas = 0
+//decimas de segundo
 setInterval(()=>{
 
-    ReactDOM.createRoot(document.getElementById('app')).render(<Home contador={segundos}/>);
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home decimasdesegundo={decimasdesegundo} segundos={segundos} minutos={minutos} horas={horas}/>);
     
+    decimasdesegundo ++
+
+    (decimasdesegundo == 60? decimasdesegundo=0 : decimasdesegundo );
+   
+},10)
+//segundos
+setInterval(() => {
+
+    (decimasdesegundo == 99 ? segundos = 1: decimasdesegundo);
     segundos ++
-
-    (segundos == 60 ? segundos=0 : segundos );
-
+    (segundos == 60 ? segundos =0 : segundos);
     
-
-},1000)
-
-
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home contador={segundos}/>);
-
+}, 1000);
+//minutos
+setInterval(() => {
+    (segundos == 59 ? minutos = 1 : segundos);
+    minutos ++
+    (minutos == 60 ? minutos = 0 : minutos)
+},60000 );
