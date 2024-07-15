fdb2f23ce79df529bbf7360577cab06ef9ba2af3
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-07-15 18:35:40 +0000

    [Updated : home.jsx // template.html] home.jsx: Añadido estilo. template.html : añadido fontawesome

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,23 +6,29 @@ import React from "react";
 //create your first component
 const Home = (props) => {
 	return (
-		<div className="container mt-5">
+		<div className="container mt-5 ">
 			<div className="row ">
-				<div className="col-lg-4"></div>
-				<div className="col-lg-1  border border-3" style={{backgroundColor:"#e9ecef"}}>
-					<h1 className="text-center mt-5">{props.decimasdesegundo}</h1>
-				</div>	
-				<div className="col-lg-1 border border-3" style={{backgroundColor:"#e9ecef"}}>
-					<h1 className="text-center mt-5">{props.segundos}</h1>
-				</div>	
-				<div className="col-lg-1  border border-3" style={{backgroundColor:"#e9ecef"}}>
-					<h1 className="text-center mt-5">{props.minutos}</h1>
-				</div>	
-				<div className="col-lg-1  border border-3 " style={{backgroundColor:"#e9ecef"}}>
-					<h1 className="text-center mt-5">{props.horas}</h1>
-				</div>	
+				<div className=" col-lg-3"></div>
+				<div className="col-2 col-lg-1 d-flex justify-content-center  border border-1 border border-light rounded " style={{backgroundColor:"#D6DBDF"}}>
+					<h1 className="mt-5 text-center"style={{marginBottom:"40px", marginLeft:"10px", marginRight:"10px"}}><i className="fa-solid fa-clock"></i></h1>
 				</div>
-				<div className="col-lg-4"></div>
+				<div className="col-2 col-lg-1 d-flex justify-content-center  border border-1 border border-light rounded " style={{backgroundColor:"#82E0AA"}}>
+					<h1 className="mt-5 text-center">{props.decimasdesegundo}</h1>
+				</div>	
+				<div className="col-2 col-lg-1 d-flex justify-content-center border border-1 border-light rounded" style={{backgroundColor:"#ABEBC6 "}}>
+					<h1 className=" mt-5">{props.segundos}</h1>
+				</div>	
+				<div className="col-2 col-lg-1 d-flex justify-content-center border border-1 border-light rounded" style={{backgroundColor:"#D5F5E3"}}>
+					<h1 className="mt-5">{props.minutos}</h1>
+				</div>	
+				<div className="col-2 col-lg-1 d-flex justify-content-center border border-1 border-light rounded " style={{backgroundColor:"#EAFAF1"}}>
+					<h1 className=" mt-5">{props.horas}</h1>
+				</div>	
+				<div className="col-2 col-lg-1 d-flex justify-content-center  border border-1 border border-light rounded " style={{backgroundColor:"#D6DBDF"}}>
+					<h1 className="mt-5 text-center"style={{marginBottom:"40px", marginLeft:"10px", marginRight:"10px"}}><i className="fa-solid fa-clock"></i></h1>
+				</div>
+			</div>
+				<div className="col-lg-3"></div>
 			</div>
 	);
 };
